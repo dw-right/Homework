a40b968c4c4dbf8effe5eed049e902ec5b91e672
dw-right <578486558@qq.com>
2018-01-04 20:11:43 +0800

    丁伟作业

--- DingWei/one/src/ding/Example.java
+++ DingWei/one/src/ding/Example.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("请输入一个字符串:");
-        String pw=scanner.nextLine();
-        char[] a=pw.toCharArray();
-        for(int i=0;i<a.length;i++){
+        Scanner scanner=new Scanner(System.in);   //创建输入流扫描器
+        System.out.println("请输入一个字符串:");    //提示信息
+        String pw=scanner.nextLine();               //获取输入
+        char[] a=pw.toCharArray();                  //转换为字符数组
+        for(int i=0;i<a.length;i++){                //便利数组
             a[i]=(char)(a[i]^5);
         }
         System.out.println("加密后:");
